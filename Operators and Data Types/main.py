b280a94fe4f50a062fba4a8bea1ca72cbac3e0cb
SCodingal <tchiynamelo11@gmail.com>
2024-11-23 17:26:22 +0100

    Add files via upload

---
+++ Operators and Data Types/main.py
@@ -0,0 +1,27 @@
+#Variables and data types
+name="Tchiyna"
+age=13
+height=1.63
+is_student=True
+print("my name is",name)
+print("my age is",age)
+print("my height is",height)
+print("am I a student?",is_student)
+print(type(name))
+#typecasting-conversern of one data tye to the other
+print(float(age))
+print(int(height))
+#operators
+num1=2
+num2=3
+print("sum:",(num1+num2))
+print("mult:",(num1*num2))
+print("sub:",(num1-num2))
+print("div:",(num1/num2))
+print("floor division:",(num1//num2))
+print("square:",(num1**num2))
+#string operations
+var="scarlet"
+print(var[0])
+print(var[-1])
+print(len(var))
