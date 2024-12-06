9b15dde486a9aca08c2a810fbef001425d7050ad
SCodingal <tchiynamelo11@gmail.com>
2024-12-06 16:34:34 +0100

    for loop and while loop

---
+++ For and While Loop/shape.py
@@ -0,0 +1,14 @@
+var= int(input("enter the number of terms:"))
+for i in range(var+1):
+    print(i)
+
+for i in range(2,var+1,2):
+    print(i)
+
+for i in range(1,var+1,2):
+    print(i)
+
+
+string="flower"
+for j in string:
+    print(j)
