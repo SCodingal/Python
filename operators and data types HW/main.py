621f15340e707d5014d538dd4adea95d647caf81
SCodingal <tchiynamelo11@gmail.com>
2024-12-07 16:04:15 +0100

    operators and data types hw

---
+++ operators and data types HW/main.py
@@ -0,0 +1,17 @@
+
+num1=4
+num2=2
+num3=1
+print((num1))
+print((num2))
+print((num3))
+
+
+if num1>num2:
+  print("this number if greater then that one")
+
+elif num1>num3:
+   print("this number if greater then that one")
+
+else:
+   print("this number is smaller")
