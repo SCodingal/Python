aab5cc00ca3c5f0667a122dc11b59a06a44d03a6
SCodingal <tchiynamelo11@gmail.com>
2024-12-07 15:51:42 +0100

    operators and functions HW

---
+++ operators and functions HW/main.py
@@ -0,0 +1,11 @@
+name="Tchiyna Melo"
+age=13
+is_student=True
+print(" hello my name is",name)
+print("my  actual age is",age)
+print("am I a student?",is_student)
+
+print(type(name))
+
+print(float(age))
+
