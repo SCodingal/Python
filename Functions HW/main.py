38b6d581af3f5068ec89bbbe9c5c73e85595804d
SCodingal <tchiynamelo11@gmail.com>
2024-12-07 17:30:00 +0100

    Functions HW

---
+++ Functions HW/main.py
@@ -0,0 +1,21 @@
+def add(t,l):
+    return t+l
+
+t=int(input("enter a number"))
+
+l=int(input("enter a number"))
+print(add(t,l))
+
+def multiply(m,h):
+    return m*h
+
+m=int(input("enter a number"))
+
+h=int(input("enter a number"))
+print(multiply(m,h))
+
+
+
+
+
+
