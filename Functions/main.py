e359be2bc4948a02650064fa2d33c983659ad469
SCodingal <tchiynamelo11@gmail.com>
2024-12-07 17:13:12 +0100

    Functions

---
+++ Functions/main.py
@@ -0,0 +1,15 @@
+def greet(message):
+    print(message)
+
+greet("Happy Diwali")
+greet("Happy new year")
+greet("happy birthday")
+greet("Happy thanksgiving")
+
+def subtract(a,b):
+    return a-b
+
+a=int(input("enter a number"))
+
+b=int(input("enter a number"))
+print(subtract(a,b))
