c55640e3cb8042a49463a2edc7474fe79ae0bae0
SCodingal <tchiynamelo11@gmail.com>
2024-12-20 17:42:52 +0100

    opps-2 HW

---
+++ opps-2HW/main.py
@@ -0,0 +1,26 @@
+class circle():
+    def __init__(self,p,t,r):
+       self.pie=p
+       self.two=t
+       self.radius=r
+
+    def circleircle_perimiter(self):
+        return self.radius*self.two*self.pie
+    
+newcircle = circle(3.14,2,2)
+print("Dimension of Circle - Length : %d Height : %d"% (newcircle.length, newcircle.height))
+print("Area of Circle :", newcircle.circle_area())
+
+class circle():
+    def __init__(self,p,t,r):
+       self.pie=p
+       self.two=t
+       self.radius=r
+
+    def circleircle_area(self):
+        return self.radius*self.two*self.pie
+    
+newcircle = circle(3.14,2,2)
+print("Dimension of Circle - Length : %d Height : %d"% (newcircle.length, newcircle.height))
+print("Area of Circle :", newcircle.circle_area())
+
