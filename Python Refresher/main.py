5122b276b1989350065340deaf03e98ae30b69d6
SCodingal <tchiynamelo11@gmail.com>
2024-12-27 17:05:25 +0100

    python Refresher HW

--- Python Refresher/main.py
+++ Python Refresher/main.py
@@ -12,7 +12,7 @@ for i in range(6,0,-1):#for row
   print()
 
 print("Number /n")
-for i in range(1,6):#for row
+for i in range(1,11,):#for row
   for j in range(i):
     print(i, end=" ")
   print()
