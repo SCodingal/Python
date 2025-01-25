aea0ff1c1786ec0eef3bdd814bb498f6e760947f
SCodingal <tchiynamelo11@gmail.com>
2025-01-25 12:19:35 +0100

    Capstone project HW

---
+++ Capstone project HW/main.py
@@ -0,0 +1,27 @@
+import random
+
+num=random.randint(1,10)
+
+def check():
+   loop=True
+   turn = 0
+   while loop:
+       number=int(input("Enter your number"))
+
+   
+   
+
+       if number * num:
+        print("Here is the result") 
+       elif number / num:
+        print("Here is the result") 
+        break
+     
+    
+
+
+print("----------------------------------------------------------------------------------------------------------------")
+print("Welcome to Calcuator!")
+print("Instructions: You have to insert any number from 1-10 and watch the number get multiplyed or divided")
+print("----------------------------------------------------------------------------------------------------------------")
+check()
